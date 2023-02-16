7c21b5e7990ea2047a79ff4bf2e4697b7669e407
JayaMadhuriK <JayaMadhuri5603@gmail.com>
2023-02-17 01:44:17 +0530

    division of two numbers

---
+++ Divide.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Divide
+{
+  public static void main(String args[])
+  {
+   float a,b;
+   Scanner sc=new Scanner(System.in);
+   System.out.println("enter a and b values");
+   a=sc.nextFloat();
+   b=sc.nextFloat();
+   System.out.println(a/b);
+  }
+}
