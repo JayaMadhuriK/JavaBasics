122b6b647f59420508a6442c00efa0ca2d14d845
JayaMadhuriK <JayaMadhuri5603@gmail.com>
2023-02-17 01:40:50 +0530

    display hello and your name

---
+++ Name.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Name
+{
+   public static void main(String args[])
+   {
+    String s;
+    Scanner sc= new Scanner(System.in);
+    System.out.println("enter name to display");
+    s=sc.nextLine();
+    System.out.println("HELLO");
+    System.out.println(s);
+   }
+}
