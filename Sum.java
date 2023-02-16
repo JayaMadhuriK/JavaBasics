2ff975508b6f75ea2b9bffcdb8a61692f043246f
JayaMadhuriK <JayaMadhuri5603@gmail.com>
2023-02-17 01:45:22 +0530

    Sum of two numbers

--- Sum.java
+++ Sum.java
@@ -3,11 +3,11 @@ class Sum
 {
   public static void main(String args[])
   {
-   int a,b;
+   float a,b;
    Scanner sc = new Scanner(System.in);
    System.out.println("enter a and  b values");
-   a=sc.nextInt();
-   b=sc.nextInt();
+   a=sc.nextFloat();
+   b=sc.nextFloat();
    System.out.println(a+b);
   }
 }
