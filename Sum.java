96823134b4b8c1d801b33d6af4d9e92df5cdd13f
JayaMadhuriK <JayaMadhuri5603@gmail.com>
2023-02-17 01:35:59 +0530

    sum of two integers

---
+++ Sum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Sum
+{
+  public static void main(String args[])
+  {
+   int a,b;
+   Scanner sc = new Scanner(System.in);
+   System.out.println("enter a and  b values");
+   a=sc.nextInt();
+   b=sc.nextInt();
+   System.out.println(a+b);
+  }
+}
