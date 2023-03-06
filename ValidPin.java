4e81fec7ab20dd68e8ea6c39b34322b2f6426b04
JayaMadhuriK <JayaMadhuri5603@gmail.com>
2023-03-06 17:26:13 +0530

    pincode validation usign regex

--- ValidPin.java
+++ ValidPin.java
@@ -1,17 +1,24 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.regex.*;
 class ValidPin{
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
-		String pin1;
-		String pin2="123456";
+
+		String regex = "^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$";
+		
+		Pattern p = Pattern.compile(regex);
+
 		System.out.println("enter the pin: ");
-		pin1 = sc.next();
-		if(pin1.equals(pin2)==true){
-			System.out.println("pin is valid");
+		String pincode = sc.nextLine();
+
+		if(pincode == null){
+			System.out.println("invalid pincode");
 		}
-		else{
-			System.out.println("pin is invalid");
-		}
+
+		Matcher m = p.matcher(pincode);
+		
+		System.out.println(m.matches());
+		sc.close();
 	}
 }
 		
