2f8b1838f01a7a64b9c78de421f00afc11dd0e91
JayaMadhuriK <JayaMadhuri5603@gmail.com>
2023-03-07 17:46:18 +0530

    adding Math.PI and rounding decimals

--- Question4/RunTest.java
+++ Question4/RunTest.java
@@ -2,29 +2,36 @@ import java.util.*;
 
 abstract class Shape{
 
-    abstract double perimeter(double a, double b);
-    abstract double area(double a, double b);
+    abstract double perimeter();
+    abstract double area();
 
 }
 
 class Circle extends Shape{
-
-    double perimeter(double pie,double radius ){
-        return 2*pie*radius;
+    double radius;
+    public Circle(double radius){
+        this.radius = radius;
+    }
+    double perimeter(){
+        return 2*Math.PI*radius;
     }
 
-    double area(double pie,double radius){
-        return pie*radius*radius;
+    double area(){
+        return Math.PI*radius*radius;
     }
 }
 
 class Rectangle extends Shape{
-
-    double perimeter(double length, double breadth){
+    double length, breadth;
+    public Rectangle(double length, double breadth){
+        this.length = length;
+        this.breadth = breadth;
+    }
+    double perimeter(){
         return 2*(length+breadth);
     }
 
-    double area(double length, double breadth){
+    double area(){
         return length*breadth;
     }
 }
@@ -32,31 +39,29 @@ class Rectangle extends Shape{
 public class RunTest{
     public static void main(String args[])
     {
-        Circle cir = new Circle();
-        Rectangle rec = new Rectangle();
         Scanner sc = new Scanner(System.in);
-
         System.out.println("enter circle radius");
         double radius=sc.nextDouble();
-        double pie=3.14;
-
+        Circle cir = new Circle(radius);
+       
         System.out.println("perimeter of the circle: ");
-        double c = cir.perimeter(pie,radius);
-        System.out.format("%.3f",c);
+        double b = cir.perimeter();
+        System.out.println(Math.round(b*100.0)/100.0);
 
         System.out.println("\narea of the circle: ");
-        System.out.println(cir.area(pie,radius));
+        double d = cir.area();
+        System.out.println(Math.round(d*100.0)/100.0);
 
-
-        System.out.println("enter length and breadth of rectangle");
+        System.out.println("\nenter length and breadth of rectangle");
         double length=sc.nextDouble();
         double breadth=sc.nextDouble();
+        Rectangle rec = new Rectangle(length,breadth);
 
         System.out.println("perimeter of the rectangle: ");
-        System.out.println(rec.perimeter(length,breadth));
+        System.out.println(rec.perimeter());
 
-        System.out.println("area of the rectangle: ");
-        System.out.println(rec.area(length,breadth));
+        System.out.println("\narea of the rectangle: ");
+        System.out.println(rec.area());
         sc.close();
     }
 }
