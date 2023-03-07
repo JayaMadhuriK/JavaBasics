3fbf72b155248fcf9e8c56c7f7e53df36c93eb3c
JayaMadhuriK <JayaMadhuri5603@gmail.com>
2023-03-07 23:40:21 +0530

    Arithmetic operators

---
+++ ArthOperators.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class ArthOperators{
+	public static void main(String args[]){
+		int a = 10, b = 20;
+		System.out.println("Arithmetic operators");
+		System.out.println("addition: "+(a+b));
+		System.out.println("subtraction: "+(a-b));
+		System.out.println("multiplication: "+(a*b));
+		System.out.println("divison: "+(a/b));
+		System.out.println("remainder: "+(a%b));
+	}
+}
