f67d0e3d6dbbda9e6f09cacd3c982b5722c1aadd
JayaMadhuriK <JayaMadhuri5603@gmail.com>
2023-03-31 12:33:27 +0530

    Store Unique elements in list

--- List/Unique.java
+++ List/Unique.java
@@ -2,18 +2,32 @@ import java.util.*;
 class Unique {
     public static void main(String args[]){
         List<Integer> list = new ArrayList<>();
-        HashSet<Integer> hash = new HashSet<>();
+       
         list.add(10);
         list.add(20);
         list.add(10);
         list.add(10);
         list.add(30);
-        hash.addAll(list);
+        list.add(40);
+        list.add(30);
+        list.add(20);
+        List<Integer> list1 = new ArrayList<>();
+       
+       /* HashSet<Integer> hash = new HashSet<>(list);
         list.clear();
-        list.addAll(hash);
-        
-        
+        list.addAll(hash); */
+        for(int i = 0; i < list.size(); i++){
+      
+         if(list1.contains(list.get(i))){
+           list.remove(i);
+         }
+         else{
+          list1.add(list.get(i));
+         }
+          
+        }
       System.out.println(list);
+      System.out.println(list1);
       
     } 
 }
