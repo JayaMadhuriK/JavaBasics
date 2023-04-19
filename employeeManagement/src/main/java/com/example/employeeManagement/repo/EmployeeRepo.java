560cc7cb7748deefc82e66aff00b0205cedd250c
JayaMadhuri <jayamadhuri5603@gmail.com>
2023-04-19 23:53:58 +0530

    deleted

--- employeeManagement/src/main/java/com/example/employeeManagement/repo/EmployeeRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.employeeManagement.repo;
-
-import com.example.employeeManagement.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepo extends JpaRepository<Employee, Integer>{
-
-}
